b1c6edcc2fe87cd4055df440ecdcbff85da8b1f5
youngkeunyi <touryklee100363@gmail.com>
2026-08-15 02:06:40 +0900

    Deploy K-MediWell frontend to GitHub Pages

--- gencow/runtime.js
+++
@@ -1,4 +0,0 @@
-import { createCrudFactory, createGencowHttpRouteBuilders, createGencowProcedureBuilders, } from "@gencow/core";
-export const procedure = createGencowProcedureBuilders();
-export const httpRoute = createGencowHttpRouteBuilders();
-export const createCrud = createCrudFactory(procedure);
--- src/main.js
+++
@@ -1,9 +0,0 @@
-import { jsx as _jsx } from "react/jsx-runtime";
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import { GencowProvider } from "@gencow/react";
-import { apiClient } from "./lib/gencow";
-import { LocaleProvider } from "./lib/i18n";
-import App from "./App";
-import "./index.css";
-createRoot(document.getElementById("root")).render(_jsx(StrictMode, { children: _jsx(LocaleProvider, { children: _jsx(GencowProvider, { apiClient: apiClient, children: _jsx(App, {}) }) }) }));
